88d360ccaa4effd9ea4b09312c7885f98392109b
NierRep24 <danielsanchezmartinez064@gmail.com>
2025-07-31 17:32:41 -0500

    puertos  endpoint del profe

--- frontend_react/src/components/Contact_form.jsx
+++ frontend_react/src/components/Contact_form.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import ReCAPTCHA from "react-google-recaptcha";
 
 
-const API_URL = 'http://localhost:3227'
+const API_URL = 'http://167.172.150.250:3240'
 const SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
 
 const ContactForm = () => {

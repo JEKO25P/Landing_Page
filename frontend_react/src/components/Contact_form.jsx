c2200f5be753f304b2d135f832fa68da7641d529
NierRep24 <danielsanchezmartinez064@gmail.com>
2025-08-01 17:27:50 -0500

    url efdiaz

--- frontend_react/src/components/Contact_form.jsx
+++ frontend_react/src/components/Contact_form.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import ReCAPTCHA from "react-google-recaptcha";
 
 
-const API_URL = 'http://167.172.150.250:3227'
+const API_URL = 'http://3227.efdiaz.xyz'
 const SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
 
 const ContactForm = () => {
--- frontend_react/src/components/Contact_table.jsx
+++ frontend_react/src/components/Contact_table.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const API_URL = 'http://167.172.150.250:3227'
+const API_URL = 'http://3227.efdiaz.xyz'
 
 const estados = ['nuevo', 'contactado', 'descartado']
 
